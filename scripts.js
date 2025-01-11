50e6378d73ab68fa4a2413c9ddeaaa1530c4d6ad
Jayaprakash <jayaprakashjp3232@gmail.com>
2025-01-11 15:23:02 +0530

    1st commit

---
+++ scripts.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const words = ["full stack developer", "data analyst", "writer", "gamer"];
+    let i = 0;
+    const typingSpeed = 100;
+    const erasingSpeed = 50;
+    const delayBetweenWords = 2000;
+    const changingText = document.getElementById("changing-text");
+
+    function typeWriter(text, index) {
+        if (index < text.length) {
+            changingText.textContent += text.charAt(index);
+            index++;
+            setTimeout(() => typeWriter(text, index), typingSpeed);
+        } else {
+            setTimeout(() => eraseText(text), delayBetweenWords);
+        }
+    }
+
+    function eraseText(text) {
+        if (text.length > 0) {
+            changingText.textContent = text.slice(0, -1);
+            setTimeout(() => eraseText(text.slice(0, -1)), erasingSpeed);
+        } else {
+            i = (i + 1) % words.length;
+            setTimeout(() => typeWriter(words[i], 0), typingSpeed);
+        }
+    }
+
+    typeWriter(words[i], 0);
+});
+
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.style.width = sidebar.style.width === "250px" ? "0" : "250px";
+}
